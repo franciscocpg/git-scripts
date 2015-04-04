d03fa100b572aefb5277a20cbc684a0af48d4501
Francisco Guimarães <francisco.guimaraes@tidexa.com>
2015-04-04 09:38:36 -0300

    ajustes

--- git-init.sh
+++ git-init.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 # Script de inicialização de ambiente debian/ubuntu
+BASH_RC=~/.bashrc
+GIT_IGNORE_GLOBAL_HOME=~/.gitignore_global
+FOLDER=$(pwd)
 
 # Ignorar globalmente
-cp .gitignore_global ~
-git config --global core.excludesfile ~/.gitignore_global
+# cp .gitignore_global ~ 
+# Testar com link simbólico ao invés de copiar
+if [[ ! -e "$GIT_IGNORE_GLOBAL_HOME" ]]; then
+	ln -s "$FOLDER/.gitignore_global" "$GIT_IGNORE_GLOBAL_HOME"
+	git config --global core.excludesfile "$GIT_IGNORE_GLOBAL_HOME"
+fi
+
 
 # Adiciona o path para execução dos comandos
-FOLDER=$(pwd)
-echo "GIT_TIDEXA_SCRIPTS=$FOLDER" >> ~/.bashrc
-echo 'PATH=${PATH}:$GIT_TIDEXA_SCRIPTS' >> ~/.bashrc
+CONTAINS_GIT_TIDEXA_SCRIPTS=$(cat $BASH_RC | grep GIT_TIDEXA_SCRIPTS)
+if [[ -z "$CONTAINS_GIT_TIDEXA_SCRIPTS" ]]; then
+	echo "GIT_TIDEXA_SCRIPTS=$FOLDER" >> $BASH_RC
+	echo 'PATH=${PATH}:$GIT_TIDEXA_SCRIPTS' >> $BASH_RC	
+fi
+
+./git-init-config
