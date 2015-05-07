8cee3b53e77508db8723671cc5fe01f7dbe1e894
Francisco Guimarães <francisco.guimaraes@tidexa.com>
2015-05-07 13:54:08 -0300

    Inclusão readPassword

--- utils.sh
+++ utils.sh
@@ -104,3 +104,38 @@ ask() {
 chd() {
 	cd "$1" -> /dev/null
 }
+
+# Lê uma senha colocando '*' para os caracteres digitados
+readPassword() {
+	unset PASSWORD
+	unset CHARCOUNT
+
+	stty -echo
+
+	CHARCOUNT=0
+	while IFS= read -p "$PROMPT" -r -s -n 1 CHAR
+	do
+	    # Enter - accept password
+	    if [[ $CHAR == $'\0' ]] ; then
+	        break
+	    fi
+	    # Backspace
+	    if [[ $CHAR == $'\177' ]] ; then
+	        if [ $CHARCOUNT -gt 0 ] ; then
+	            CHARCOUNT=$((CHARCOUNT-1))
+	            PROMPT=$'\b \b'
+	            PASSWORD="${PASSWORD%?}"
+	        else
+	            PROMPT=''
+	        fi
+	    else
+	        CHARCOUNT=$((CHARCOUNT+1))
+	        PROMPT='*'
+	        PASSWORD+="$CHAR"
+	    fi
+	done
+
+	stty echo
+
+	echo $PASSWORD
+}
