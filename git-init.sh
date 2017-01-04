bbf896a5319e50597311137a9d625e55094daa10
Francisco Guimarães <francisco.cpg@gmail.com>
2017-01-04 10:28:54 -0200

    Remove unused config from init

--- git-init.sh
+++ git-init.sh
@@ -20,32 +20,8 @@ else
 	installSoft meld
 fi
 
-
-installSoft wget
-
-# Ignorar globalmente
-# cp .gitignore_global ~ 
-if [[ ! -e "$GIT_IGNORE_GLOBAL_HOME" ]]; then
-	ln -s "$FOLDER/.gitignore_global" "$GIT_IGNORE_GLOBAL_HOME"
-	git config --global core.excludesfile "$GIT_IGNORE_GLOBAL_HOME"
-fi
-
 # Color output
 git config --global color.ui always
 git config --global color.status always
-
-# Adiciona o path para execução dos comandos
-CONTAINS_GIT_TIDEXA_SCRIPTS=$(cat $BASH_RC | grep GIT_TIDEXA_SCRIPTS | true)
-if [[ -z "$CONTAINS_GIT_TIDEXA_SCRIPTS" ]]; then
-	echo -e "\nGIT_TIDEXA_SCRIPTS=$FOLDER" >> $BASH_RC
-	echo 'PATH=${PATH}:$GIT_TIDEXA_SCRIPTS' >> $BASH_RC	
-fi
-
-# Instalação do certificado GITLAB
-if [[ "$OSTYPE" == *linux* ]]; then
-	sudo wget https://www.startssl.com/certs/sub.class1.server.ca.pem -O /usr/share/ca-certificates/mozilla/StartCom_Class_1_Primary_Intermediate_Server_CA.crt
-	sudo sh -c 'echo "mozilla/StartCom_Class_1_Primary_Intermediate_Server_CA.crt" >> /etc/ca-certificates.conf'
-	sudo update-ca-certificates
-fi
 
 ./git-init-config "false"
